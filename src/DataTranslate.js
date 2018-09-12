6b69080dc82e53ce307e1d2607a62ce6424fc53b
Mathieu Dutour <mat.dutour@gmail.com>
2018-09-12 18:03:28 -0400

    add handler to reset the translation

--- src/DataTranslate.js
+++ src/DataTranslate.js
@@ -1,7 +1,8 @@
 const sketch = require('sketch')
-const { DataSupplier } = sketch
+const { DataSupplier, Settings } = sketch
 const util = require('util')
 
+const SETTING_KEY = 'plugins.example.data-translate.originalText'
 const YANDEX_API_KEY = "trnsl.1.1.20180912T154941Z.a526333405a74b23.f809d9991b4d9e56bf40c95e7d5325513669a25b"
 const API_URL = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${YANDEX_API_KEY}&lang=en-ru&text=`
 
@@ -19,13 +20,26 @@ export function onSupplyTranslation (context) {
   let dataKey = context.data.key
   const items = util.toArray(context.data.items).map(sketch.fromNative)
   items.forEach((item, index) => {
-    let text = item.text
-    let url = API_URL + encodeURI(item.text)
+    let text = Settings.layerSettingForKey(item, SETTING_KEY)
+    if (!text) {
+      Settings.setLayerSettingForKey(item, SETTING_KEY, item.text)
+      text = item.text
+    }
+    let url = API_URL + encodeURI(text)
     fetch(url)
       .then(response => response.json() )
       .then(json => {
         DataSupplier.supplyDataAtIndex(dataKey, json.text[0], index)
       })
       .catch(e => console.error(e))
+  })
+}
+
+export function resetTranslation () {
+  sketch.getSelectedDocument().selectedLayers.forEach(item => {
+    let text = Settings.layerSettingForKey(item, SETTING_KEY)
+    if (text) {
+      item.text = text
+    }
   })
 }
