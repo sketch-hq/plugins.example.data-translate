052b86942f60c16a3a69527b49ef08c259e53cfa
Ale M <bomberstudios@gmail.com>
2018-09-14 20:36:33 +0200

    Detect source language, and randomise target

--- src/DataTranslate.js
+++ src/DataTranslate.js
@@ -1,9 +1,10 @@
 const sketch = require('sketch')
-const { DataSupplier } = sketch
+const { DataSupplier, UI } = sketch
 const util = require('util')
+const config = {}
 
 const YANDEX_API_KEY = "trnsl.1.1.20180912T154941Z.a526333405a74b23.f809d9991b4d9e56bf40c95e7d5325513669a25b"
-const API_URL = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${YANDEX_API_KEY}&lang=en-ru&text=`
+const API_URL = `https://translate.yandex.net/api/v1.5/tr.json/`
 
 export function onStartup () {
   // Register the plugin
@@ -16,16 +17,42 @@ export function onShutdown () {
 }
 
 export function onSupplyTranslation (context) {
-  let dataKey = context.data.key
-  const items = util.toArray(context.data.items).map(sketch.fromNative)
-  items.forEach((item, index) => {
-    let text = item.text
-    let url = API_URL + encodeURI(item.text)
-    fetch(url)
-      .then(response => response.json() )
-      .then(json => {
-        DataSupplier.supplyDataAtIndex(dataKey, json.text[0], index)
+  getAvailableLanguages()
+    .then(langs => {
+      UI.message('🕑 Translating…')
+      let dataKey = context.data.key
+      const items = util.toArray(context.data.items).map(sketch.fromNative)
+      items.forEach((item, index) => {
+        detectLanguageFor(item.text)
+          .then(sourceLanguage => {
+            let targetLanguage = langs[Math.floor(Math.random()*langs.length)]
+            const url = `${API_URL}translate?key=${YANDEX_API_KEY}&lang=${sourceLanguage}-${targetLanguage}&text=${encodeURI(item.text)}`
+            fetch(url)
+              .then(response => response.json())
+              .then(json => {
+                DataSupplier.supplyDataAtIndex(dataKey, json.text[0], index)
+                UI.message(null)
+              })
+          })
       })
-      .catch(e => console.error(e))
+    }).catch(e => console.error(e))
+}
+
+function getAvailableLanguages(){
+  return new Promise(function(resolve, reject){
+    fetch(`${API_URL}getLangs?key=${YANDEX_API_KEY}&ui=en`)
+      .then(response => response.json())
+      .then(json => resolve(Object.keys(json.langs)))
+  })
+}
+
+function detectLanguageFor(text){
+  return new Promise(function(resolve, reject){
+    let detectURL = `${API_URL}detect?key=${YANDEX_API_KEY}&text=${encodeURI(text)}`
+    fetch(detectURL)
+    .then(response => response.json())
+    .then(json => {
+      resolve(json.lang)
+    })
   })
 }
